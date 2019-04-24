4c85b23814dda908e78185b4c21b096633708918
Eva Lecocq <evalecocq@free.fr>
2019-04-24 11:38:29 +0200

    update souris

---
+++ Les Nobles/Assets/Script/NavTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTest : MonoBehaviour {
+
+    private NavMeshAgent myNMA;
+
+	// Use this for initialization
+	void Start () {
+        myNMA = GetComponent<NavMeshAgent>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit yes;
+        if (Physics.Raycast(ray, out yes, 10f))
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                myNMA.destination = yes.point;
+            }
+        }
+    }
+}
