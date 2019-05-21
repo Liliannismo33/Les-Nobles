7a90ba66f7c779dc4f7680e088801b15d408ca12
ArmandGemeux <armandgemeux@hotmail.fr>
2019-05-21 11:09:58 +0100

    ça marche enfin bvefudnsb,cod

--- Les Nobles/Assets/Script/ActionsScripts/InteractionController.cs
+++ Les Nobles/Assets/Script/ActionsScripts/InteractionController.cs
@@ -8,9 +8,9 @@ public class InteractionController : MonoBehaviour {
 
     //public List<HighlightedObject> highlightedObject = new List<HighlightedObject>();
     //public HighlightedObject[] highlightedObject;
+    //public ParticleSystem particleSysteme;
 
     public HighlightedObject highlightedObject; // Variable qui attends le script permettant d'allumer l'outliner
-    //public ParticleSystem particleSysteme;
     public GameObject getTarget; // Variable qui attends le GameObject touché par le RayCast
     public int stepState; // État actuel de l'événement
 
@@ -28,13 +28,13 @@ public class InteractionController : MonoBehaviour {
         {
             highlightedObject = getTarget.GetComponent<HighlightedObject>(); 
             highlightedObject/*[0]*/.launchOutliner(); //Allume l'outliner de l'objet actuellement ciblé
-            /*if (getTarget.CompareTag("PetiteFille"))
+            if (getTarget.CompareTag("PetiteFille"))
             {
                 Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward, Color.red, 5f);
-                if (!particleSysteme.isPlaying)
-                    particleSysteme.Play();
+                //if (!particleSysteme.isPlaying)
+                    //particleSysteme.Play();
             }
-            */
+            
             if (OVRInput.GetDown(OVRInput.Button.One))
             {
                 if (getTarget.CompareTag("PetiteFille")) // Si le RayCast touche un objet dont le Tag est PetiteFille, alors joue un code précis
@@ -42,12 +42,12 @@ public class InteractionController : MonoBehaviour {
                     if (stepState == 0) // Si l'étape de l'événement est la première étape...
                     {
                         Debug.Log("Passage à l'étape suivante, guignol");
-                        stepState++; // ... Alors l'événement passe à l'étape suivante
+                        stepState++; // ... alors l'événement passe à l'étape suivante
                     }
                     else
                     {
                         // ici on souhaite faire parler la petite fille (car le joueur intéragit avec elle)
-                        Debug.Log("Tu parle à la petite fille");
+                        Debug.Log("Tu parles à la petite fille");
                     }
                 }
             }
@@ -69,5 +69,4 @@ public class InteractionController : MonoBehaviour {
         }
         return null;
     }
-
 }
