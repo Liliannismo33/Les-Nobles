f23e5447f7d3d55a5fa0a9c98532a0527c0bbf5f
Léna Brasseau <lena.brasseau@gmail.com>
2019-06-20 14:47:29 +0200

    Test

---
+++ Les Nobles/Assets/Script/ActionsScripts/TriggerPhoneRing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPhoneRing : MonoBehaviour {
+
+    public Light phoneLight;
+    public Color color01 = Color.green;
+    private Color color02;
+
+    // Use this for initialization
+    void Start () {
+        color02 = phoneLight.GetComponent<Light>().color;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("In Trigger");
+        phoneLight.GetComponent<Light>().intensity = 6;
+        phoneLight.GetComponent<Light>().color = color01;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Out Trigger");
+        phoneLight.GetComponent<Light>().intensity = 1;
+        phoneLight.GetComponent<Light>().color = color02;
+    }
+}
